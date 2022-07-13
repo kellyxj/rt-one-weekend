a3264dc009c2baf8f7918b4a9fc210f431d908f0
Kelly Jiang <kllyjiang@gmail.com>
2022-07-13 14:10:59 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    int nx = 200;
+    int ny = 100;
+    std::ofstream output("image.ppm");
+    output << "P3\n" << nx << " " << ny << "\n255\n";
+    for(int j = 0; j < ny; j++) {
+        for(int i = 0; i < nx; i++) {
+            float r = float(i)/float(nx);
+            float g = float(j)/float(ny);
+            float b = .2;
+            int ir = int(255.99 * r);
+            int ig = int (255.99 * g);
+            int ib = int(255.99 * b);
+            output << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    output.close();
+}
